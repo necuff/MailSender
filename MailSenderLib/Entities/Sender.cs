4611ca9565587cb1fbb5c664121591d2d232605f
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-02-04 00:58:24 +0700

    Сущности сервера и отправителя

---
+++ MailSenderLib/Entities/Sender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSenderLib.Entities
+{
+    public class Sender
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ MailSenderLib/Entities/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSenderLib.Entities
+{
+    /// <summary>
+    /// Почтовый сервер
+    /// </summary>
+    public class Server
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public bool UseSSL { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
