eb49fed790aea04c4038f0a22627da489f9ae06a
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-02-05 23:12:48 +0700

    Добавление MVVMLight

---
+++ MailSenderWpf/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace MailSenderWpf.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<MainWindowViewModel>();
+        }
+
+        public MainWindowViewModel MainWindowModel => ServiceLocator.Current.GetInstance<MainWindowViewModel>();
+    }
+}
