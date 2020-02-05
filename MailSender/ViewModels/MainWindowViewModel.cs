f81e7f4c4588a271515f906285e35d5b241ac396
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-02-05 22:43:38 +0700

    add ViewModel && MVVM

---
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailSenderLib.MVVM;
+
+namespace WpfTest.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Наше новое окно!";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+            /*
+            set
+            {
+                if (Equals(_Title, value)) return;                 
+                _Title = value;
+                OnPropertyChanged("title");
+                OnPropertyChanged();
+                
+            }
+            */
+        }
+    }
+}
