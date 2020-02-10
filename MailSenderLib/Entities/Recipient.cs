e2f36f91ee8af58f4e4330a4312ff1ca5b167b29
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-02-10 23:53:58 +0700

    Собственные правила валидации

--- MailSenderLib/Entities/Recipient.cs
+++ MailSenderLib/Entities/Recipient.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using MailSenderLib.Entities.Base;
 
 namespace MailSenderLib.Entities
 {
-    public class Recipient : PersonEntity
-    {        
-       
+    public class Recipient : PersonEntity, IDataErrorInfo
+    {
+        public override string Name 
+        { 
+            get => base.Name;
+            set
+            {
+                if (value == "Иванов123")
+                    throw new ArgumentException("Иванов123 нам не подходит", nameof(value));
+                base.Name = value;
+            }
+        }
+
+        string IDataErrorInfo.this[string PropertyName] 
+        { 
+            get
+            {
+                switch (PropertyName)
+                {
+                    default: return null;
+                    case nameof(Name):
+                        var name = Name;
+                        if (name is null) return "Пустыя ссылка на имя";
+                        if (name.Length <= 2) return "Имя должно быть длиннее двух символов";
+                        if (name.Length > 20) return "Длина имени должна быть не больше 20 символов";
+
+                        return null;                        
+                }
+            }
+        }
+
+        string IDataErrorInfo.Error => null;
     }
 }
---
+++ MailSenderWpf/Infrastructure/ValidationRules/RegExValidation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace MailSenderWpf.Infrastructure.ValidationRules
+{
+    class RegExValidation : ValidationRule
+    {
+        private Regex _Regex;
+
+        public string Pattern
+        {
+            get => _Regex.ToString();
+            set => _Regex = string.IsNullOrWhiteSpace(value) ? null : new Regex(value);
+        }
+
+        public bool AllowNull { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is null)
+                return AllowNull
+                    ? ValidationResult.ValidResult
+                    : new ValidationResult(false, ErrorMessage ?? "Отсутствует ссылка на строку");
+            if (_Regex is null) return ValidationResult.ValidResult;
+
+            if (!(value is string str))
+                str = value.ToString();
+
+            return _Regex.IsMatch(str)
+                ? ValidationResult.ValidResult
+                : new ValidationResult(false, ErrorMessage ?? $"Строка не удовлетворяет регулярному выражению {_Regex}");
+        }
+    }
+}
