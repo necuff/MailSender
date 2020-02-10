f62e0f06f9dc38cfd0c9c3694a7777c95b1bec7f
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-02-11 00:23:23 +0700

    Первый модульный тест

---
+++ Tests/MailSenderLib.Tests/Service/TextEncoderTests.cs
@@ -0,0 +1,29 @@
+﻿using MailSenderLib.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSenderLib.Tests.Service
+{
+    [TestClass]
+    public class TextEncoderTests
+    {
+        [TestMethod]
+        public void Encode_ABC_to_BCD_with_key_1()
+        {
+            //A-A-A Arrange - Act - Assert
+            
+            //Arrange
+            const string str = "ABC";
+            const int key = 1;
+            const string expected_str = "BCD";
+
+            //Act
+            var actual_str = TextEncoder.Encode(str, key);
+
+            //Assert
+            Assert.AreEqual(expected_str, actual_str);
+        }
+    }
+}
