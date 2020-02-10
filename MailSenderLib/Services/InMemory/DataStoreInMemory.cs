5ee3f1663ee5e2c0d345660cadad1286d7f57c7b
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-02-10 22:47:54 +0700

    Переработка сервиса хранилища данных получателей писем в памяти

---
+++ MailSenderLib/Services/InMemory/DataStoreInMemory.cs
@@ -0,0 +1,44 @@
+﻿using MailSenderLib.Data;
+using MailSenderLib.Entities;
+using MailSenderLib.Entities.Base;
+using MailSenderLib.Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MailSenderLib.Services
+{
+    public abstract class DataStoreInMemory<T> : IDataStore<T> where T: BaseEntity
+    {
+        private readonly List<T> _Items = new List<T>();
+
+        protected DataStoreInMemory(List<T> Items = null) => _Items = Items ?? new List<T>();
+
+
+        public int Create(T item)
+        {
+            if (_Items.Contains(item)) return item.Id;
+            item.Id = _Items.Count == 0 ? 1 : _Items.Max(r => r.Id) + 1;
+            _Items.Add(item);
+            return item.Id;
+        }
+
+        public abstract void Edit(int id, T item);        
+
+        public IEnumerable<T> GetAll() => _Items;
+
+        public T GetByID(int id) => _Items.FirstOrDefault(item => item.Id == id);
+
+        public T Remove(int id)
+        {
+            var item = GetByID(id);
+            if (item != null)
+                _Items.Remove(item);
+            return item;
+        }
+
+        public void SaveChanges()
+        {
+            System.Diagnostics.Debug.WriteLine("Сохранение изменений в хранилище {0}.", typeof(T));
+        }
+    }
+}
--- MailSenderLib/Services/InMemory/RecipientsStoreInMemory.cs
+++ MailSenderLib/Services/InMemory/RecipientsStoreInMemory.cs
@@ -6,11 +6,16 @@ using System.Linq;
 
 namespace MailSenderLib.Services
 {
-    public class RecipientsStoreInMemory : IRecipientsStore
-    {
-        public IEnumerable<Recipient> GetAll() => TestData.Recipients;
-        
-        public void Edit(int id, Recipient recipient)
+
+
+    public class RecipientsStoreInMemory : DataStoreInMemory<Recipient>, IRecipientsStore
+    {            
+        public RecipientsStoreInMemory() : base(TestData.Recipients)
+        {
+
+        }
+
+        public override void Edit(int id, Recipient recipient)
         {
             //Делаем вид, что работаем с БД, а не с объектами в памяти            
             //recipient.id <> id, т.к. это разные объекты
@@ -19,30 +24,6 @@ namespace MailSenderLib.Services
 
             db_recipient.Name = recipient.Name;
             db_recipient.Address = recipient.Address;
-        }
-
-        public Recipient GetByID(int id) => TestData.Recipients.FirstOrDefault(r => r.Id == id);
-
-        public int Create(Recipient Recipient)
-        {
-            if (TestData.Recipients.Contains(Recipient)) return Recipient.Id;
-            Recipient.Id = TestData.Recipients.Count == 0 ? 1 : TestData.Recipients.Max(r => r.Id) + 1;
-            TestData.Recipients.Add(Recipient);
-            return Recipient.Id;
-        }
-
-        public Recipient Remove(int id)
-        {
-            var db_recipient = GetByID(id);
-            if(db_recipient != null)
-                TestData.Recipients.Remove(db_recipient);
-            return db_recipient;
-        }
-
-        public void SaveChanges()
-        {
-            System.Diagnostics.Debug.WriteLine("Сохранение изменений в хранилище получателей писем.");
-            //Т.к. это хранилище данных в памяти, то здесь ничего не делаем
-        }
+        }       
     }
 }
