74e839b6774aa45aa01c4355bc49f4e5a8515eb7
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-02-13 23:46:25 +0700

    Рефакторинг классов почты

---
+++ MailSenderLib/Services/MailSenderDebug.cs
@@ -0,0 +1,21 @@
+﻿using MailSenderLib.Entities;
+using System.Diagnostics;
+
+namespace MailSenderLib.Services
+{
+    public class MailSenderDebug
+    {
+        private Server _Server;
+
+        public MailSenderDebug(Server Server) => _Server = Server;
+        
+        public void Send(Mail Mail, Sender From, Recipient To)
+        {
+            Debug.WriteLine("Отправка почты от {0} к {1} через {2}:{3}[{4}]\r\n{5}:{6}",
+                From.Address, To.Address,
+                _Server.Address, _Server.Port, _Server.UseSSL ? "SSL" : "no SSL",
+                Mail.Subject, Mail.Body);
+        }
+    }
+
+}
