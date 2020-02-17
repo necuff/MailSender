e4bdab55dbc4c2141c8099430fd8fa0a3618499f
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-02-17 22:35:19 +0700

    Асинхронная рассылка почты

--- MailSenderLib/Services/MailSender.cs
+++ MailSenderLib/Services/MailSender.cs
@@ -5,6 +5,8 @@ using System.Net.Mail;
 using System.Net;
 using MailSenderLib.Entities;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace MailSenderLib.Services
 {
@@ -37,6 +39,37 @@ namespace MailSenderLib.Services
         {
             foreach (var recipient in To)
                 ThreadPool.QueueUserWorkItem(_ => Send(Message, From, recipient));
+        }
+
+        public async Task SendAsync(Mail Mail, Sender From, Recipient To)
+        {
+            using (var message = new MailMessage(new MailAddress(From.Address, From.Name), new MailAddress(To.Address, To.Name)))
+            {
+                message.Subject = Mail.Subject;
+                message.Body = Mail.Body;
+
+                var login = new NetworkCredential(_Server.Login, _Server.Password);
+                using (var client = new SmtpClient(_Server.Address, _Server.Port) { EnableSsl = _Server.UseSSL, Credentials = login })
+                    //client.Send(message);
+                    await client.SendMailAsync(message).ConfigureAwait(false);
+            }
+        }
+
+        /*
+         * Вариант 1
+        public async Task SendAsync(Mail Message, Sender From, IEnumerable<Recipient> To)
+        {
+            await Task.WhenAll(To.Select(to => SendAsync(Message, From, To))).ConfigureAwait(false);
+        }
+        */
+        public async Task SendAsync(Mail Message, Sender From, IEnumerable<Recipient> To, CancellationToken Cancel = default)
+        {
+            foreach (var recipient in To)
+            {
+                Cancel.ThrowIfCancellationRequested();
+                await SendAsync(Message, From, recipient).ConfigureAwait(false);
+            }
+                
         }
 
     }
