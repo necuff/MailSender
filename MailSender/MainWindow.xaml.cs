61b82bd3c1174ad9f77c4ccd364dcadb7c3bff46
Mikhail Zaykov <necuff@users.noreply.github.com>
2020-02-17 21:53:27 +0700

    пример зависшего приложения WPF

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Net;
 using System.Net.Mail;
 using MailSenderLib.MVVM;
+using System.Threading;
 
 namespace MailSender
 {
@@ -23,6 +24,33 @@ namespace MailSender
     /// </summary>
     public partial class WpfMailSender : Window 
     { 
-        public WpfMailSender() => InitializeComponent(); 
+        public WpfMailSender() => InitializeComponent();
+
+        private void OnStartButtonClick(object sender, RoutedEventArgs e)
+        {
+            const string message = "KIjfkdsjf";
+
+            var result = GetMessageLenght(message);
+
+            Result.Text = result.ToString();
+        }
+
+        private int _StartCount;
+        private void OnCancelButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private Task<int> GetMessageLenghtAsync(string Message, int Timeout = 30)
+        {
+            return Task.Run(() => GetMessageLenght(Message, Timeout));
+        }
+
+        private int GetMessageLenght(string Message, int Timeout = 30)
+        {
+            for (int i = 0; i < 100; i++)
+                Thread.Sleep(Timeout);
+            return Message.Length + _StartCount++;
+        }
     }
 }
